b496a1d8492d46ed45200a228164dbac948a0b71
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-05-17 21:57:57 +0900

    add .html and .js file

---
+++ addAndRemoveElements.js
@@ -0,0 +1,11 @@
+function onAddListItemButtonClick(){
+    let addListItemInput = document.getElementById("addListItemInput");
+    let addListItemInputText = addListItemInput.value;
+    
+    let listItem = document.createElement("li");
+    listItem.textContent = addListItemInputText;
+    listItem.classList.add("blueText");
+
+    let skillList = document.getElementById("skillList");
+    skillList.appendChild(listItem);
+}
